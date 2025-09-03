a67f1075b548c9a53fa5e8ab10753c7e31550cef
Deepa <deepabaskar0108@gmail.com>
2025-09-03 16:12:57 +0530

    routing fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -21,18 +21,37 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./components/project3/project3.component').then(m => m.Project3Component),
     children: [
-      { path: 'login', loadComponent: () => import('./components/project3/auth/login.component').then(m => m.LoginComponent) },
-      { path: 'register', loadComponent: () => import('./components/project3/auth/register.component').then(m => m.RegisterComponent) },
-      { path: 'notes', loadComponent: () => import('./components/project3/notes/notes-list.component').then(m => m.NotesListComponent), canActivate: [notesLoginAuthGuard] },
-      { path: 'notes/edit', loadComponent: () => import('./components/project3/notes/note-edit.component').then(m => m.NoteEditComponent), canActivate: [notesLoginAuthGuard] },
-//       { 
-//   path: 'notes/edit/:id',
-//   loadComponent: () => import('./components/project3/notes/note-edit.component').then(m => m.NoteEditComponent),
-//   canActivate: [notesLoginAuthGuard],
-//   data: {
-//     renderMode: 'server'
-//   }
-// },
+      {
+        path: 'login',
+        loadComponent: () =>
+          import('./components/project3/auth/login.component').then(m => m.LoginComponent)
+      },
+      {
+        path: 'register',
+        loadComponent: () =>
+          import('./components/project3/auth/register.component').then(m => m.RegisterComponent)
+      },
+      {
+        path: 'notes',
+        loadComponent: () =>
+          import('./components/project3/notes/notes-list.component').then(m => m.NotesListComponent),
+        canActivate: [notesLoginAuthGuard],
+        data: { renderMode: 'client' }
+      },
+      {
+        path: 'notes/edit',
+        loadComponent: () =>
+          import('./components/project3/notes/note-edit.component').then(m => m.NoteEditComponent),
+        canActivate: [notesLoginAuthGuard],
+        data: { renderMode: 'client' }
+      },
+      {
+        path: 'notes/edit/:id',
+        loadComponent: () =>
+          import('./components/project3/notes/note-edit.component').then(m => m.NoteEditComponent),
+        canActivate: [notesLoginAuthGuard],
+        data: { renderMode: 'client' }
+      },
       { path: '', redirectTo: 'login', pathMatch: 'full' },
     ]
   }
